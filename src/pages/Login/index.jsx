216726f297663bb923be415ff1fc17e8e0280bb4
mvaleck <minivaleck@hotmail.com>
2025-06-16 15:34:49 -0300

    feat: logar usuários cadastrados

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
 import img from "../../assets/date.png"
 import {Container, LoginSection } from "./style"
-import { Link } from "react-router-dom"; 
-
+import { Link, useNavigate} from "react-router-dom"; 
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
 
 function Login () {
+
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    if (!email || !senha) {
+      alert("Preencha todos os campos para contiuar..")
+      return;
+    }
+    signInWithEmailAndPassword(auth, email, senha)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log("Usuário logado: ", user);
+        alert("Login realizado com sucesso");
+        navigate('/home');
+      })
+      .catch((error) => {
+        console.error("Erro ao logar: ", error);
+        alert("Erro ao fazer login: " + error.message);
+      });
+  };
+
   return (
     <Container>
       <section>
@@ -12,9 +37,18 @@ function Login () {
 
       <LoginSection>
         <h1>Login</h1>
-        <input type="email" name="" id=""placeholder="E-mail" />
-        <input type="password" name="" id="" placeholder="Senha" />
-        <button type="button">Entrar</button>
+        
+        <input type="email" 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          name="" id=""placeholder="E-mail" />
+        
+        <input type="password"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+          placeholder="Senha" />
+        
+        <button type="button" onClick={handleLogin}>Entrar</button>
         <Link to="/cadastro">ou Cadastre-se aqui </Link>
       </LoginSection>
     </Container>
