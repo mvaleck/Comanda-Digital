15ebb75d6ae69a2505c3c191d84e811dfc19c27f
mvaleck <minivaleck@hotmail.com>
2025-06-16 15:22:31 -0300

    feat: cadastrar usuários

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyCjTBJRknO8iCOaZPBN1Nysax7hTh267zk",
   authDomain: "comanda-digital-54132.firebaseapp.com",
@@ -19,7 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const auth = getAuth(app); // 👈 adicione esta linha
+const analytics = getAnalytics(app);
 
-export { auth }; // 👈 e esta também!
+export { app, auth }; // 👈 e esta também!
--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,18 +1,61 @@
 import {Container, LoginSection} from "../Login/style"
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import img from "../../assets/cadastro.png"
+import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
 
 function Cadastro () {
+
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const navigate = useNavigate();
+
+  const handleCadastar = () => {
+    if (!nome || !email || !senha) {
+      alert("Preencha todos os campos!"); 
+      return;
+    } 
+    createUserWithEmailAndPassword(auth, email, senha)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log('Uauário Cadastrado.', user);
+        alert (`Bem vindo(a), ${nome}.. Cadastro realizado com sucesso`);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.error('Erro no cadastro', error);
+        alert("Erro ao cadastrar: " + error.message);
+      });
+  };
+
+
+
+
+
   return (
     <div>
       <Container>
                     
         <LoginSection>
           <h1>Cadastre-se</h1>
-          <input type="text" placeholder="Nome do Estabelecimento"/>
-          <input type="email" name="" id=""placeholder="E-mail" />
-          <input type="password" name="" id="" placeholder="Senha" />
-          <button type="button">Cadastrar</button>
+          <input type="text"
+            value={nome}
+            onChange={(e)=> setNome(e.target.value)}
+            placeholder="Nome do Estabelecimento"/>
+
+          <input type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            name="" id=""placeholder="E-mail" />
+         
+          <input type="password" 
+            value={senha}
+            onChange={(e)=> setSenha(e.target.value)}
+            name="" id="" placeholder="Senha" />
+         
+          <button type="button" onClick={handleCadastar}>Cadastrar</button>
           <Link to="/">Já tem Cadastro? Faça seu Login aqui</Link>
         </LoginSection>
         <section>
